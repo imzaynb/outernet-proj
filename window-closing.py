504cdcf7fe224d6758fbaa8436650b779d83267a
stati30241 <staticsahib30241@gmail.com>
2023-07-29 09:27:54 -0400

    Now it minimizes all the windows

--- window-closing.py
+++ window-closing.py
@@ -1,9 +1,11 @@
-import pyautogui
+import win32gui, win32con
 
-def close_window():
-    button_rect = pyautogui.locateOnScreen("x_button.png", confidence=0.8, grayScale=True)
-    if not button_rect: continue
+def callback(hwnd, extra):
+    if win32gui.IsWindowVisible(hwnd):
+        print(f"Window text: '{win32gui.GetWindowText(hwnd)}'")
 
-    button_center = pyautogui.center()
-    pyautogui.click(x=button_center.x, y=button_center.y)
+def minimize_everything(hwnd, extra):
+    if win32gui.IsWindowVisible(hwnd)and win32gui.GetWindowText(hwnd) != '':
+        win32gui.ShowWindow(hwnd, win32con.SW_MINIMIZE)
 
+win32gui.EnumWindows(minimize_everything, None)
